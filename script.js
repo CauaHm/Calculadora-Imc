81592a1a4a392408a491838f7feaa03bcad402e1
Cauã_mMatias <144502865+CauaHm@users.noreply.github.com>
2025-01-01 16:03:25 -0300

    Adicionando o codigo

---
+++ script.js
@@ -0,0 +1,26 @@
+function calcularImc() {
+    let nome = document.getElementById('nome').value
+    let altura = document.getElementById('altura').value
+    let peso = document.getElementById('peso').value
+    let resultado = document.querySelector('.result')
+
+    altura = parseFloat(altura)
+    peso = parseFloat(peso)
+
+    let imc = peso / (altura * altura)
+
+    let mensagem = `${nome}, seu IMC é ${imc.toFixed(2)}. `
+
+    if(imc < 18.5) {
+        mensagem += 'Você está abaixo do peso.'
+    } else if(imc >=18.5 && imc <= 24.9) {
+        mensagem += 'Você está com o peso normal.'
+    } else if(imc>=25 && imc <=29.9 ) {
+        mensagem += 'Você está com sobrepeso.'
+    } else {
+        mensagem += 'Você está com obesidade.'
+    }
+
+    resultado.innerHTML = mensagem
+    resultado.style.height = '200px'
+}
